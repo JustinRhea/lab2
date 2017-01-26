ee4ca783f615a41c8e91be1f5d48a40444db5156
Justin <justinrhea96@gmail.com>
2017-01-26 15:19:15 -0600

    1.0

---
+++ WriteFile.h
@@ -0,0 +1,31 @@
+#if !defined WRITE_FILE
+#define WRITE_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class WriteFile{
+   private:
+      ofstream output_file;
+      bool closed;
+   public:
+      WriteFile(const char* file_name);
+      ~WriteFile();
+      void writeLine(String* line);
+      void close();
+};
+/*struct WriteFile
+{
+   ofstream output_file;
+   bool closed;
+};
+
+WriteFile* createWriteFile(const char* file_name);
+void destroyWriteFile(WriteFile* wf);
+void writeLine(WriteFile* wf, String* line);
+void close(WriteFile* wf);
+*/
+
+#endif
